90c806e5e0784477ac37a82d025412b3e93ab4e2
Joey Liu <joeyliu224@gmail.com>
2020-01-16 00:13:34 -0500

    planner table created

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const parser = require('body-parser');
 
+app.use(express.static('./dist'))
 app.use(parser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import {Draggable,Droppable} from 'react-drag-and-drop';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-          Learn React
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
 
-      </header>
-    </div>
-  );
+  componentDidMount(){
+    console.log(this.state)
+    console.log('hi')
+  };
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Weekly Planner</h1>
+        <div>
+          <table>
+            <tr>
+              <th>Monday</th>
+              <th>Tuesday</th>
+              <th>Wednesday</th>
+              <th>Thursday</th>
+              <th>Friday</th>
+            </tr>
+            <tr>
+              <td>
+                <div>hi</div>
+                <div>yo</div>
+                <div>yo</div>
+                <div>yo</div>
+                <div>yo</div>
+                <div>yo</div>
+                <div>yo</div>
+                <div>yo</div>
+                <div>yo</div>
+                <div>yo</div>
+                <div>yo</div>
+                <div>yo</div>
+                <div>yo</div>
+              </td>
+              <td>
+                <div>a</div>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </table>  
+        </div>
+      </div>
+    );
+
+  }
+  
+  
 }
 
 export default App;
