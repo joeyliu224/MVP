ce54142f9be0e393a721d53e2403e8f9ac6a39db
Joey Liu <joeyliu224@gmail.com>
2020-01-17 10:39:25 -0500

    minor changes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const parser = require('body-parser');
-const db = require('../db/index.js');
+//const db = require('../db/index.js');
 const path = require('path');
 
 app.use(express.static('./dist'))
@@ -12,27 +12,27 @@ app.use(parser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/api/todos',(req,res) => {
-  //console.log(req.body)
-  db.save(req.body).then(
-    () => {res.sendStatus(201)}
-  ).catch((err) => {
-    res.sendStatus(500)
-  });
+// app.post('/api/todos',(req,res) => {
+//   //console.log(req.body)
+//   db.save(req.body).then(
+//     () => {res.sendStatus(201)}
+//   ).catch((err) => {
+//     res.sendStatus(500)
+//   });
 
-})
+// })
 
-app.get('api/todos',(req,res) => {
-  db.retreive('todo').then(
-    (data) =>{
-      console.log('data:',data)
-      res.status(200).send(data);
-    }
-  ).catch(
-    (err) => {
-      res.sendStatus(404);
-    }
-  )
-})
+// app.get('api/todos',(req,res) => {
+//   db.retreive('todo').then(
+//     (data) =>{
+//       console.log('data:',data)
+//       res.status(200).send(data);
+//     }
+//   ).catch(
+//     (err) => {
+//       res.sendStatus(404);
+//     }
+//   )
+// })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
